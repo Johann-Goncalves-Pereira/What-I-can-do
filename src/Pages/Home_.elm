4b2fd255ee6df79e3b87f2ae86000423bf465199
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-01-29 21:07:01 -0300

    feat: weather block

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -2,7 +2,7 @@ module Pages.Home_ exposing (Model, Msg, page)
 
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route exposing (Route)
-import Html exposing (Html, a, article, b, br, div, h1, h2, h3, img, main_, p, section, span, strong, text)
+import Html exposing (Html, a, article, b, br, div, em, h1, h2, h3, img, main_, p, section, span, strong, text)
 import Html.Attributes exposing (attribute, class, id, src, style)
 import Html.Events exposing (onClick)
 import Page exposing (Page)
@@ -61,8 +61,17 @@ view model =
     , body =
         UI.layout model.route
             "home"
-            [ section [ class "ctnr" ]
-                [ h1 [ class "ctnr__title" ] [ text "I am love Html & Css" ]
+            [ section [ class "shallow--down ctnr weather" ]
+                [ p [ class "ctnr__text weather__txt--n" ]
+                    [ strong [ class "ctnr__text__title" ] [ text "26" ]
+                    , b [ class "ctnr__text__medium" ] [ text "Clear" ]
+                    ]
+                , p [ class "weather__txt--s" ]
+                    [ em [] [ text "Curitiba City, Brasil" ]
+                    , text "position: fixed; bottom: 2em; right: 2em; width: calc(42px + 3ch); height: 36px; background-color: rgb(18, 147, 216); "
+                    ]
                 ]
+
+            -- , section [ class "shallow--up" ] []
             ]
     }
