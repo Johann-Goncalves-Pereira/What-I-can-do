62d16820525a089e464ae3ba5c814cdc337f7638
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-01-30 14:28:08 -0300

    feat: bar svg gradient buttons

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -2,12 +2,13 @@ module Pages.Home_ exposing (Model, Msg, page)
 
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route exposing (Route)
-import Html exposing (Html, a, article, b, br, div, em, h1, h2, h3, img, main_, p, section, span, strong, text)
+import Html exposing (Html, a, article, b, br, button, div, em, h1, h2, h3, img, main_, p, section, span, strong, text)
 import Html.Attributes exposing (attribute, class, id, src, style)
 import Html.Events exposing (onClick)
 import Page exposing (Page)
 import Request exposing (Request)
 import Shared
+import Svg.ElmSvg as ESvg
 import UI
 import View exposing (View)
 
@@ -70,6 +71,22 @@ view model =
                     [ em [] [ text "Curitiba City, Brasil" ]
                     , text "position: fixed; bottom: 2em; right: 2em; width: calc(42px + 3ch); height: 36px; background-color: rgb(18, 147, 216); "
                     ]
+                ]
+            , div [ class "ctnr__btm--fc" ]
+                [ button [ class "shallow--up btm" ] [ ESvg.home ESvg.SvgInvertArrows ]
+                , button [ class "shallow--up btm" ] [ ESvg.home ESvg.SvgPlus ]
+                ]
+            , div [ class "ctnr__btm--fr" ]
+                [ button [ class "shallow--up btm" ] [ ESvg.home <| ESvg.SvgArrow ESvg.Right ]
+                , button [ class "shallow--up btm" ] [ ESvg.home <| ESvg.SvgArrow ESvg.Left ]
+                , button [ class "shallow--up btm" ] [ ESvg.home ESvg.SvgFlag ]
+                ]
+            , section [ class "shallow--down ctnr__btm--fr line-btm" ]
+                [ button [ class "line-btm__btm" ] [ ESvg.home ESvg.SvgDollar ]
+                , button [ class "line-btm__btm" ] [ ESvg.home ESvg.SvgPerson ]
+                , button [ class "line-btm__btm" ] [ ESvg.home ESvg.SvgShoppingCart ]
+                , button [ class "line-btm__btm" ] [ ESvg.home ESvg.SvgInformation ]
+                , button [ class "line-btm__btm shallow--up btm" ] [ em [] [ text "Change" ] ]
                 ]
 
             -- , section [ class "shallow--up" ] []
