1fde792b8620c81218acd17f1d435dba7dcefcee
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-05-05 23:04:33 -0300

    feat: add lime list random e other stuff

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,5 +1,6 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
+import Array exposing (Array)
 import Browser.Dom as BrowserDom exposing (Element, Error)
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route
@@ -9,10 +10,12 @@ import Html.Attributes.Aria exposing (ariaLabel, ariaLabelledby)
 import Layout exposing (pageConfig)
 import Page
 import Platform exposing (Task)
+import Random
 import Request
 import Shared
 import Svg exposing (desc)
 import Task
+import Time exposing (Posix, every, millisToPosix, posixToMillis)
 import Utils.View exposing (customProps)
 import View exposing (View)
 
@@ -32,13 +35,22 @@ page shared req =
 
 
 type alias Model =
-    { headerSize : Int }
+    { headerSize : Int
+    , titleIndex : Int
+    , timePass : Int
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { headerSize = 64 }
-    , BrowserDom.getElement "root__header" |> Task.attempt GotHeader
+    ( { headerSize = 64
+      , titleIndex = 0
+      , timePass = 0
+      }
+    , Cmd.batch
+        [ BrowserDom.getElement "root__header" |> Task.attempt GotHeader
+        , run GetTitle
+        ]
     )
 
 
@@ -49,6 +61,11 @@ init =
 type Msg
     = -- Header
       GotHeader (Result Error Element)
+      -- Title
+    | GetTitle
+    | NewTitle Int
+      -- Typing
+    | TypingTime
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,6 +79,20 @@ update msg model =
                 Ok element ->
                     ( { model | headerSize = round element.element.height }, Cmd.none )
 
+        GetTitle ->
+            ( model, Random.generate NewTitle (Random.int 0 <| List.length introTitles - 1) )
+
+        NewTitle index ->
+            ( { model | titleIndex = index }, Cmd.none )
+
+        TypingTime ->
+            ( { model | timePass = model.timePass + 1 }, Cmd.none )
+
+
+run : Msg -> Cmd Msg
+run m =
+    Task.perform (always m) (Task.succeed ())
+
 
 
 -- SUBSCRIBE
@@ -69,10 +100,11 @@ update msg model =
 
 subs : Model -> Sub Msg
 subs model =
-    Sub.none
+    Time.every 60 (\_ -> TypingTime)
 
 
 
+-- every (toFloat (posixToMillis model.pos)) (always TypingTime)
 -- VIEW
 
 
@@ -89,7 +121,49 @@ viewLayout model =
         { pageConfig
             | route = Route.Home_
             , mainAttrs =
-                [ customProps [ { prop = "header-height", value = String.fromInt model.headerSize ++ "px" } ]
+                [ customProps
+                    [ { prop = "header-height"
+                      , value = String.fromInt model.headerSize ++ "px"
+                      }
+                    ]
                 ]
-            , mainContent = []
+            , mainContent = [ viewIntro model ]
         }
+
+
+introTitles : List String
+introTitles =
+    [ "Hello, world!"
+    , "Hello, universe!"
+    , "Hello, universe and everything!"
+    , "Hello, universe and everything and everything!"
+    , "Hello, universe and everything and everything and everything!"
+    , "Hello, universe and everything and everything and everything and everything!"
+    ]
+
+
+viewIntro : Model -> Html Msg
+viewIntro model =
+    let
+        nameHeader : String
+        nameHeader =
+            "intro-head"
+
+        getPossibleTitles : Int -> String
+        getPossibleTitles index =
+            Array.fromList introTitles
+                |> Array.get index
+                |> Maybe.withDefault "Hi, I am a robot!"
+
+        slicer : Int -> Int -> String
+        slicer index typing =
+            getPossibleTitles index
+                |> String.slice 0 typing
+    in
+    section [ class "intro", ariaLabelledby nameHeader ]
+        [ h1 [ class "intro__title", id nameHeader ]
+            [ slicer model.titleIndex 50
+                |> text
+            ]
+        , text <| String.fromInt model.timePass
+        ]
