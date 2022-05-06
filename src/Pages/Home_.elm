61b75ef2d7d733c48c097173c7cf0dcf827bf40b
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-05-05 21:18:23 -0300

    feat: home start

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,6 +1,6 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
-import Components.Svg as SVG exposing (Logo(..))
+import Browser.Dom as BrowserDom exposing (Element, Error)
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route
 import Html exposing (Html, a, div, h1, h2, h5, section, text)
@@ -8,18 +8,22 @@ import Html.Attributes exposing (class, href, id, rel, tabindex, target)
 import Html.Attributes.Aria exposing (ariaLabel, ariaLabelledby)
 import Layout exposing (pageConfig)
 import Page
+import Platform exposing (Task)
 import Request
 import Shared
 import Svg exposing (desc)
+import Task
+import Utils.View exposing (customProps)
 import View exposing (View)
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
 page shared req =
-    Page.sandbox
+    Page.element
         { init = init
         , update = update
         , view = view
+        , subscriptions = subs
         }
 
 
@@ -28,12 +32,14 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { headerSize : Int }
 
 
-init : Model
+init : ( Model, Cmd Msg )
 init =
-    {}
+    ( { headerSize = 64 }
+    , BrowserDom.getElement "root__header" |> Task.attempt GotHeader
+    )
 
 
 
@@ -41,14 +47,29 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = -- Header
+      GotHeader (Result Error Element)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            model
+        GotHeader promiseResponse ->
+            case promiseResponse of
+                Err _ ->
+                    ( model, Cmd.none )
+
+                Ok element ->
+                    ( { model | headerSize = round element.element.height }, Cmd.none )
+
+
+
+-- SUBSCRIBE
+
+
+subs : Model -> Sub Msg
+subs model =
+    Sub.none
 
 
 
@@ -67,7 +88,8 @@ viewLayout model =
     Layout.layout
         { pageConfig
             | route = Route.Home_
+            , mainAttrs =
+                [ customProps [ { prop = "header-height", value = String.fromInt model.headerSize ++ "px" } ]
+                ]
             , mainContent = []
         }
-
-
